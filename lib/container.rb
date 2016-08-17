d7cd4ee2904a84e42b5df78e805c08ca968ccf85
Zachary Cutler <zachlcutler@gmail.com>
2016-08-17 16:42:29 -0400

    blah

--- lib/container.rb
+++ lib/container.rb
@@ -1,26 +1,30 @@
 class Container
-  attr_reader :maximum_holding_weight, :weight, :ingredients
+  attr_reader :ingredients, :maximum_holding_weight, :container_weight
 
   def initialize(container_weight, maximum_holding_weight)
-    MAXIMUM_HOLDING_WEIGHT = maximum_holding_weight
-    CONTAINER_WEIGHT = container_weight
+    @maximum_holding_weight = maximum_holding_weight
+    @container_weight = container_weight
     @weight = container_weight
     @ingredient = nil
     @ingredients = []
   end
 
+  def <<(ingredient)
+    @ingredients << ingredient
+  end
+
   def fill_with_ingredient(ingredient)
     @ingredient = ingredient.name
-    @weight = CONTAINER_WEIGHT + MAXIMUM_HOLDING_WEIGHT
-
     capacity = Integer(@maximum_holding_weight / ingredient.weight)
-    capacity.times do
-      @ingredients << Ingredient.new(ingredient.name,ingredient.weight)
-    end
+    capacity.times{ self << Ingredient.new(ingredient.name,ingredient.weight)}
+  end
+
+  def weight
+    (@container_weight + @ingredients.reduce(0){ |kg_sum, ingredient| kg_sum += ingredient.weight }).round
   end
 
   def remove_one_ingredient
-    @weight -= @ingredients.pop.weight
+    @ingredients.pop
   end
 
   def which_ingredient
@@ -29,7 +33,6 @@ class Container
 
   def empty
     @ingredients = []
-    @weight = CONTAINER_WEIGHT
   end
 
   def how_many_ingredients
